ce99dd33d0cf7630950ee3f89a60450c768f626a
benja <benja@maletin.(none)>
2014-08-16 17:05:12 +0200

    fix tile stitching for all zoom levels

--- journeymap.py
+++ journeymap.py
@@ -3,60 +3,50 @@ from subprocess import call
 import pdb
 
 def crop(z, x, y, xoffset, yoffset, xdest, ydest):
-	if not os.path.exists("public/tiles/slimecraft/%d/%d" % (z, xdest)):
-		os.makedirs("public/tiles/slimecraft/%d/%d" % (z, xdest))
+	if not os.path.exists("public/tiles/8/%d" % (xdest)):
+		os.makedirs("public/tiles/8/%d" % (xdest))
 
-	call("convert data/gipsyking/%d,%d.png \
-		-gravity NorthWest -crop 256x256+%d+%d \
-		public/tiles/slimecraft/%d/%d/%d.png" % (x, y, xoffset, yoffset, z, xdest, ydest),
-		shell=True)
+	if not os.path.exists("public/tiles/%d/%d/%d.png" % (z, xdest, ydest)):
+		call("convert data/day/%d,%d.png \
+			-gravity NorthWest -crop 256x256+%d+%d \
+			public/tiles/%d/%d/%d.png" % (x, y, xoffset, yoffset, z, xdest, ydest),
+			shell=True)
+	else:
+		call("convert data/day/%d,%d.png \
+			-gravity NorthWest -crop 256x256+%d+%d \
+			public/tiles/%d/%d/temp_%d.png" % (x, y, xoffset, yoffset, z, xdest, ydest),
+			shell=True)
+		call("composite \
+			public/tiles/%d/%d/temp_%d.png \
+			public/tiles/%d/%d/%d.png \
+			public/tiles/%d/%d/%d.png" % (z, xdest, ydest, z, xdest, ydest, z, xdest, ydest),
+			shell=True)
 
 
 for y in range(-129, 129):
 	print "Level y %d" % y
 	for x in range(-129, 129):
-		# source = "data/gipsyking/%d,%d.png" % (x, y)
-		# if not os.path.exists(source):
-		# 	continue
 
 		rowSize = 64
 		xdest = x + rowSize
 		ydest = -y + rowSize - 1
 
-		if not os.path.exists("public/tiles/slimecraft/7/%d" % xdest):
-			os.makedirs("public/tiles/slimecraft/7/%d" % xdest)
+		if not os.path.exists("public/tiles/7/%d" % xdest):
+			os.makedirs("public/tiles/7/%d" % xdest)
 
 		rowSize = 128
 
-		if os.path.exists("data/gipsyking/%d,%d.png" % (x, y)):
-			call("convert data/gipsyking/%d,%d.png -resize 256x256 public/tiles/slimecraft/7/%d/%d.png" % (x, y, xdest, ydest), shell=True)
+		if os.path.exists("data/day/%d,%d.png" % (x, y)):
+
+			if not os.path.exists("public/tiles/7/%d/%d.png" % (xdest, ydest)):
+				call("convert data/day/%d,%d.png -resize 256x256 public/tiles/7/%d/%d.png" % (x, y, xdest, ydest), shell=True)
+			else:
+				call("composite data/day/%d,%d.png -resize 256x256 public/tiles/7/%d/%d.png public/tiles/7/%d/%d.png" % (x, y, xdest, ydest, xdest, ydest), shell=True)
 
 			crop(8, x, y, 0, 0, x * 2 + rowSize, -y * 2 + rowSize - 1)
 			crop(8, x, y, 256, 0, x * 2 + rowSize + 1, -y * 2 + rowSize - 1)
 			crop(8, x, y, 0, 256, x * 2 + rowSize, -y * 2 + rowSize - 1 - 1)
 			crop(8, x, y, 256, 256, x * 2 + rowSize + 1, -y * 2 + rowSize - 1 - 1)
-
-		# if x % 2 == 0 and y % 2 == 0:
-		# 	rowSize = 32
-		# 	xdest = x / 2 + rowSize
-		# 	ydest = -y / 2 + rowSize - 1
-
-		# 	if not os.path.exists("public/tiles/slimecraft/6/%d" % xdest):
-		# 		os.makedirs("public/tiles/slimecraft/6/%d" % xdest)
-
-		# 	call("composite -gravity NorthWest data/gipsyking/%d,%d.png \
-		# 		-geometry 128x128+0+0 public/tiles/slimecraft/6/%d/%d.png public/tiles/slimecraft/6/%d/%d.png" % (x, y, xdest, ydest, xdest, ydest), shell=True)
-		# 	call("composite -gravity NorthWest data/gipsyking/%d,%d.png \
-		# 		-geometry 128x128+128+0 public/tiles/slimecraft/6/%d/%d.png public/tiles/slimecraft/6/%d/%d.png" % (x + 1, y, xdest, ydest, xdest, ydest), shell=True)
-		# 	call("composite -gravity NorthWest data/gipsyking/%d,%d.png \
-		# 		-geometry 128x128+0+128 public/tiles/slimecraft/6/%d/%d.png public/tiles/slimecraft/6/%d/%d.png" % (x, y + 1, xdest, ydest, xdest, ydest), shell=True)
-		# 	call("composite -gravity NorthWest data/gipsyking/%d,%d.png \
-		# 		-geometry 128x128+128+128 public/tiles/slimecraft/6/%d/%d.png public/tiles/slimecraft/6/%d/%d.png" % (x + 1, y + 1, xdest, ydest, xdest, ydest), shell=True)
-		
-
-		# if x == -1 and y == -21:
-		# 	print xdest
-		# 	pdb.set_trace()
 
 		factor = 1
 		tileSize = 256
@@ -69,19 +59,19 @@ for y in range(-129, 129):
 				xdest = x / factor + rowSize
 				ydest = -y / factor + rowSize - 1
 
-				if not os.path.exists("public/tiles/slimecraft/%d/%d" % (z, xdest)):
-					os.makedirs("public/tiles/slimecraft/%d/%d" % (z, xdest))
+				if not os.path.exists("public/tiles/%d/%d" % (z, xdest)):
+					os.makedirs("public/tiles/%d/%d" % (z, xdest))
 
 				for xoffset in range(0, factor):
 					for yoffset in range(0, factor):
-						if not os.path.exists("data/gipsyking/%d,%d.png" % (x + xoffset, y + yoffset)):
+						if not os.path.exists("data/day/%d,%d.png" % (x + xoffset, y + yoffset)):
 							continue
 
 						command = "composite -gravity NorthWest \
-							data/gipsyking/%d,%d.png \
+							data/day/%d,%d.png \
 							-geometry %dx%d+%d+%d \
-							public/tiles/slimecraft/%d/%d/%d.png \
-							public/tiles/slimecraft/%d/%d/%d.png" % (
+							public/tiles/%d/%d/%d.png \
+							public/tiles/%d/%d/%d.png" % (
 							x + xoffset, y + yoffset,
 							tileSize / factor, tileSize / factor, xoffset * tileSize / factor, yoffset * tileSize / factor,
 							z, xdest, ydest,
